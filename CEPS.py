e75da200e1554840ddc4f70793653ac75b94bd7c
AEAA17 <euricio.alexandre@gmail.com>
2024-10-18 08:18:44 -0300

    Update CEPS.py

--- CEPS.py
+++ CEPS.py
@@ -216,10 +216,10 @@ if uploaded_file is not None:
                 fig.add_hline(y=lic_xs, line_dash="dash", line_color="red")
                 # Adicionando anotações à esquerda
                 
-                fig.add_annotation(x=0, y=lsc_xr, text=f"{lsc_xr:.3f}", 
+                fig.add_annotation(x=0, y=lsc_xr, text=f"{lsc_xs:.3f}", 
                                 showarrow=False, xanchor='left', align='left', 
                                 xref='paper', yref='y', xshift=10)
-                fig.add_annotation(x=0, y=lic_xr, text=f"{lic_xr:.3f}", 
+                fig.add_annotation(x=0, y=lic_xr, text=f"{lic_xs:.3f}", 
                                 showarrow=False, xanchor='left', align='left', 
                                 xref='paper', yref='y', xshift=10)
 
